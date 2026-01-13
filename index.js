654a7dc64c2e4652c5bfe3a3712c392394de81f2
JokerJf <122100605+JokerJf@users.noreply.github.com>
2026-01-13 17:19:09 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send(`Приложение работает! Версия: ${process.env.APP_VERSION || '1.0.0'}`);
+  res.send(`Приложение работает! Версия: ${process.env.APP_VERSION || '1.0.0'} XD`);
 });
 
 app.listen(PORT, () => {
