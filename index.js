aed7f32c3d549909794c89bd88df7fa46905823c
JokerJf <122100605+JokerJf@users.noreply.github.com>
2026-01-13 16:27:54 +0500

    Update and rename server.js to index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send(`Приложение работает! Версия: ${process.env.APP_VERSION || '1.0.0'}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
